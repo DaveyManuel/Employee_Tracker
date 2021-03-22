e438e3a8f95d04293ff337ea35ba3db7646b2005
David Sanchez <davidmsanchez250@gmail.com>
2021-03-22 13:24:03 -0400

    created addRole, viewRole, addEmployee, and viewEmployee functions. Created respective const variables for questions.

--- index.js
+++ index.js
@@ -33,6 +33,43 @@ const departmentQuestions = [
         type: 'input',
         message: "What's the name of the department you would like to add?"
     }
+];
+
+const roleQuestions = [
+    {
+        name: 'role',
+        type: 'input',
+        message: 'What role would you like to add?'
+    },
+    {
+        name: 'salary',
+        type: 'input',
+        message: "Please input the role's yearly salary."
+    },
+
+];
+
+const employeeQuestions = [
+    {
+        name: 'firstName',
+        type: 'input',
+        message: 'Please tell us the first name of the employee you would like to add today.'
+    },
+    {
+        name: 'lastName',
+        type: 'input',
+        message: 'What is their last name?'
+    },
+    {
+        name: 'employeeRole',
+        type: 'input',
+        message: "What is the employee's role?"
+    },
+    {
+        name: 'employeeManager',
+        type: 'input',
+        message: "Please input the employee's manager"
+    }
 ]
 
 const startQuestions = ()=>{
@@ -41,19 +78,19 @@ const startQuestions = ()=>{
     .then(answers =>{
         switch (answers.menu) {
             case 'Add employee':
-                //call back add employee function
+                addEmployee();
                 break;
             case 'Add role':
-                //call back add employee function
+                addRole();
                 break;
             case 'Add department':
                 addDepartment();
                 break;
             case 'View employee table':
-                //call back add employee function
+                viewEmployee();
                 break;
             case 'View role table':
-                //call back add employee function
+                viewRole();
                 break;
             case 'View department table':
                 viewDepartment();
@@ -71,12 +108,23 @@ const startQuestions = ()=>{
     })
 };
 
-const viewDepartment = ()=>{
-    connection.query('SELECT * FROM department', (err, res) => {
-        if(err) throw (err);
-        console.table(res);
-        startQuestions();
-    } )
+const addRole = ()=>{
+    inquirer
+    .prompt(roleQuestions)
+    .then(answers =>{
+        connection.query('INSERT INTO role SET ?',
+        {
+            title: answers.role,
+            salary: answers.salary
+            //department_id:
+        },
+        (err,res)=>{
+            if (err) throw (err);
+            console.log(`${res.affectedRows} role inserted!\n`);
+            startQuestions();
+        }
+        )
+    })
 }
 
 const addDepartment = ()=>{
@@ -96,7 +144,49 @@ const addDepartment = ()=>{
         )
 
     })
+};
+
+const addEmployee = ()=>{
+    inquirer
+    .prompt(employeeQuestions)
+    .then(answers =>{
+        connection.query('INSERT INTO employee SET ?',
+        
+        {
+            firstName: answers.firstName,
+            lastName: answers.lastName,
+            // for ID's maybe I need an if else? If answers.employeeRole === manager || Manager then give appropriate ID
+            // role_id:
+            // manager_id:
+        }
+        )
+    })
 }
+
+const viewEmployee = () => {
+    connection.query('SELECT * FROM employee', (err,res) => {
+        if (err) throw (err);
+        console.table(res);
+        startQuestions();
+    })
+}
+
+const viewDepartment = ()=>{
+    connection.query('SELECT * FROM department', (err, res) => {
+        if(err) throw (err);
+        console.table(res);
+        startQuestions();
+    } )
+};
+
+const viewRole = ()=>{
+    connection.query('SELECT * FROM role', (err,res) => {
+        if (err) throw (err);
+        console.table(res);
+        startQuestions();
+    })
+}
+
 
 
 
